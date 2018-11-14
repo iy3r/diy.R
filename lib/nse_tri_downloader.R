c56fcb3e53f17813e15bfa1f2753e5abfc77f6cd
Vashistha Iyer <iy3rvashistha@gmail.com>
2018-11-14 11:00:22 +0530

    ...

--- lib/nse_tri_downloader.R
+++ lib/nse_tri_downloader.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(lubridate)
 library(glue)
 library(rvest)
@@ -5,7 +6,7 @@ library(rvest)
 # Earliest TR data is available for the Nifty 50 index from June 30, 1999
 DATA_AVAILABLE_FROM <- as_date("1999-06-30")
 
-URL <- function(index, from, to) {
+buildUrl <- function(index, from, to) {
   glue(
     'https://nseindia.com/products/dynaContent/equities/indices/total_returnindices.jsp?',
     'indexType={index}&',
@@ -15,13 +16,37 @@ URL <- function(index, from, to) {
     URLencode()
 }
 
-r <- read_html(URL("NIFTY%2050", DATA_AVAILABLE_FROM, as_date("2000-01-01"))) %>%
+fetchData <- function(url) {
+  url %>%
+    read_html() %>%
+    html_nodes("td") %>%
+    html_text() %>%
+    head(-2) %>%
+    matrix(ncol=2, byrow=T)
+}
+
+fromDates <- seq(ymd('1999-06-30'), today(), by = '364 days')
+toDates <- c(tail(fromDates, -1) - days(1), today())
+
+for (i in seq_along(fromDates)) {
+  print(buildUrl("NIFTY 50", fromDates[i], toDates[i]))
+}
+
+
+
+
+
+
+
+r <- 
+  buildUrl("NIFTY%2050", DATA_AVAILABLE_FROM, DATA_AVAILABLE_FROM + days(364)) %>%
+  read_html() %>%
   html_nodes("td") %>%
   html_text() %>%
   head(-2) %>%
   matrix(ncol=2, byrow=T) %>%
-  as_tibble()
+  as_tibble() %>%
+  rename(Date = V1, Value = V2) %>%
+  mutate(Date = as.Date(Date, format="%d-%b-%Y"))
 
-
-
-
+r
