977ed29631f5a7f3e4b2c62a214cf52b7a5ed032
Vashistha Iyer <iy3rvashistha@gmail.com>
2018-11-18 20:52:53 +0530

    WIP Reactive fn in Shiny

--- src/rolling_returns.R
+++ src/rolling_returns.R
@@ -2,15 +2,39 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("../lib/download_tri.R", chdir=T)
 library(timetk)
 library(PerformanceAnalytics)
+library(shiny)
 
+rollingReturns <- function(index){
+  data <- download_tri(index) 
+  
+  df <- data %>% 
+    tk_xts(select = Value, date_var = Date)
+  
+  returns <- Return.calculate(df[endpoints(df, on="months", k=1)])
+  returns <- returns[-1, ]
+  
+  charts.RollingPerformance(R = returns, width = 120)
+}
 
-data <- download_tri("NIFTY 50") 
+ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "index", 
+                  label="Index",
+                  choices=c("NIFTY 50", "NIFTY NEXT 50"), 
+                  selected="NIFTY 50")
+    ),
+    mainPanel(
+      plotOutput(outputId = "plot")
+    )
+  )
+)
 
-df <- data %>% 
-  tk_xts(select = Value, date_var = Date)
+server <- function(input, output) {
+  output$plot <- renderPlot({
+    rollingReturns(input$index)
+  })
+}
 
-returns <- Return.calculate(df[endpoints(df, on="months", k=1)])
-returns <- returns[-1, ]
-
-charts.RollingPerformance(R = returns, width = 120)
+shinyApp(ui = ui, server = server)
 
