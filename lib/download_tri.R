131617f892e49ed1dd553e768e54c86df2e4005d
Vashistha Iyer <iy3rvashistha@gmail.com>
2018-11-21 11:44:22 +0530

    Faster fetching

--- lib/download_tri.R
+++ lib/download_tri.R
@@ -3,48 +3,45 @@ library(lubridate)
 library(glue)
 library(rvest)
 
-download_tri <- function(index, logging=FALSE) {
-  buildUrl <- function(index, from, to) {
-    glue(
-      'https://nseindia.com/products/dynaContent/equities/indices/total_returnindices.jsp?',
-      'indexType={index}&',
-      'fromDate={strftime(from, "%d-%m-%Y")}&', 
-      'toDate={strftime(to, "%d-%m-%Y")}'
-    ) %>%
-      URLencode()
-  }
+parse = function(text){
+  dates = str_extract_all(text, "(?<=\")(\\d+-\\w+-\\d+)(?=\")")[[1]]
+  values = str_extract_all(text, "(?<=\")(\\d+\\.?\\d*)(?=\")")[[1]]
   
-  fetchData <- function(url) {
-    if (logging == TRUE) {
-      message(paste("Downloading -> ", url)) 
-    }
-    url %>%
-      read_html() %>%
-      html_nodes("td") %>%
-      html_text() %>%
-      head(-1) %>%
-      matrix(ncol=2, byrow=T) %>%
-      as_tibble()
-  }
-  
-  DATA_AVAILABLE_FROM <- as_date("1999-06-30")
-  fromDates <- seq(ymd('1999-06-30'), today(), by = '100 days')
-  toDates <- c(tail(fromDates, -1) - days(1), today())
-  datalist <- list()
-  
-  for (i in seq_along(fromDates)) {
-    tryCatch({
-      url <- buildUrl(index, fromDates[i], toDates[i])
-      datalist[[i]] <- fetchData(url)
-    }, warning = function(w) {
-      message(paste(w, " : ", url))
-    }, error = function(e) {
-      message(paste(e, " : ", url))
-    })
-  }
-  
-  bind_rows(datalist) %>%
-    rename(Date = V1, Value = V2) %>%
-    mutate(Date = as.Date(Date, format="%d-%b-%Y"), 
-           Value = as.numeric(Value))
+  tibble(date = dates, value = values) %>%
+    mutate(
+      date = as.Date(date, format="%d-%b-%Y"),
+      value = as.numeric(value)
+      )
 }
+
+fetch = function(index, from, to) {
+  glue(
+    'https://nseindia.com/products/dynaContent/equities/indices/total_returnindices.jsp?',
+    'indexType={index}&',
+    'fromDate={strftime(from, "%d-%m-%Y")}&', 
+    'toDate={strftime(to, "%d-%m-%Y")}'
+  ) %>% 
+    URLencode() %>%
+    read_html() %>%
+    html_nodes(xpath='//*[@id="csvContentDiv"]') %>%
+    html_text() %>%
+    parse()
+}
+
+download = function(index) {
+  start = as_date("1999-06-30")
+  end = today()
+  periods = ceiling((end - start) %>% as.numeric() / 364)
+  
+  df = tibble(date = as_date(NA), value = numeric())  
+  for(period in 1:periods) {
+    df = bind_rows(df, fetch(index, start, start + 364))
+    start = start + 365
+  }
+  
+  return(df)
+}
+
+df = download("NIFTY 50")
+
+df
